bd70a7b983115b635248d205ead7611fe386753f
sndpoffcl <sandeep2official@gmail.com>
2024-01-15 08:36:14 +0530

    OAuth implementation start

---
+++ src/main/java/dev/sandeep/UserService/security/CustomSpringUserDetailsService.java
@@ -0,0 +1,29 @@
+package dev.sandeep.UserService.security;
+
+import dev.sandeep.UserService.model.User;
+import dev.sandeep.UserService.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CustomSpringUserDetailsService implements UserDetailsService {
+    private UserRepository userRepository;
+
+    public CustomSpringUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByEmail(username);
+        if(user.isEmpty()){
+            throw new UsernameNotFoundException("User details with given username is not found");
+        }
+        User savedUser = user.get();
+        return new CustomSpringUserDetails(savedUser);
+    }
+}
---
+++ src/main/resources/db/migration/V2__addPhoneNumberToUser.sql
@@ -0,0 +1,2 @@
+ALTER TABLE ecom_user
+ADD COLUMN phoneNumber VARCHAR(255);
